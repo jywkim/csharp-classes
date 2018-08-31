8a0b4f47fdc7e25223830d1a419818c5a9b5c431
jywkim <jkim589@gmail.com>
2018-08-30 22:46:42 -0400

    open new account

--- BankAccount.cs
+++ BankAccount.cs
@@ -8,6 +8,17 @@ namespace csharp_classes
         public string Owner { get; set; }
         public decimal Balance { get; }
 
+        private static int accountNumberSeed = 1234567890;
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
         }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace csharp_classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("Jonathan", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
         }
     }
 }
